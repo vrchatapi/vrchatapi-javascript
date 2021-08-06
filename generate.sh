9c8d8369070a24e5d6b0776180ae47a5203e3304
Foorack <max@foorack.com>
2021-08-07 00:02:11 +0200

    fix: state correct license

--- generate.sh
+++ generate.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Generate Client
-#rm src docs test -rf
-#openapi-generator-cli generate \
-#-g javascript \
-#--additional-properties=projectName=vrchatapi,usePromises=true,moduleName=vrchatapi \
-#--git-user-id=vrchatapi \
-#--git-repo-id=vrchatapi-node \
-#-o . \
-#-i ../specification/openapi.yaml \
-#--http-user-agent="vrchatapi-node"
-
 rm src docs test -rf
 openapi-generator-cli generate \
 -g typescript-axios \
@@ -21,6 +10,9 @@ openapi-generator-cli generate \
 -i ../specification/openapi.yaml \
 --http-user-agent="vrchatapi-node"
 
+# Modify package.json
+sed -i 's/Unlicense/MIT/g' ./package.json
+
 # Remove unwanted lines from README
 #sed -i '/Uncomment the following line to set a prefix/d' ./README.md
 #sed -i '/apiKeyPrefix/d' ./README.md
@@ -28,7 +20,7 @@ openapi-generator-cli generate \
 #sed -i '/apiKeyPrefix/d' ./docs/*.md
 #sed -i 's/api.deleteUserById/api.getUser/g' ./README.md
 
-#sed -i 's/@default false/@default true/g' ./src/ApiClient.js
-#sed -i 's/enableCookies = false/enableCookies = true/g' ./src/ApiClient.js
+#sed -i '/Uncomment the following line to set a prefix/d' ./README.md
 
-#npm run build
+npm install
+npm run build
