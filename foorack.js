33d64c22d8ddc0e31a05386a6aeb9a5fa7c9bd74
Foorack <max@foorack.com>
2021-08-07 00:01:57 +0200

    chore: remove temporary test file

--- foorack.js
+++
@@ -1,9 +0,0 @@
-const { SystemApi } = require("./dist");
-const system = new SystemApi();
-
-system.getHealth().then(r => {
-    console.log(r.data);
-});
-system.getCurrentOnlineUsers().then(r => {
-    console.log(`Current online users: ${r.data}`);
-});
