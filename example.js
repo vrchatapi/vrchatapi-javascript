747a1f3fd15d623c8acdbb27bd938916afdcf544
Foorack <max@foorack.com>
2021-08-08 16:21:19 +0200

    fix: save cookies globally

--- example.js
+++ example.js
@@ -1,6 +1,22 @@
 const VRChatApi = require("./dist");
-const SystemApi = new VRChatApi.SystemApi();
+const configuration = new VRChatApi.Configuration({
+    username: "username",
+    password: "password"
+});
+
+const AuthenticationApi = new VRChatApi.AuthenticationApi(configuration);
+const UsersApi = new VRChatApi.UsersApi(configuration);
+const SystemApi = new VRChatApi.SystemApi(configuration);
 
 SystemApi.getCurrentOnlineUsers().then(resp => {
     console.log(`Current Online Users: ${resp.data}`);
+
+    // Calling getCurrentUser on Authentication API logs you in if the user isn't already logged in.
+    AuthenticationApi.getCurrentUser().then(resp => {
+        console.log(`Logged in as: ${resp.data.displayName}`);
+
+        UsersApi.getUser("usr_c1644b5b-3ca4-45b4-97c6-a2a0de70d469").then(resp => {
+            console.log(resp.data.displayName); // Should print out "tupper"
+        });
+    });
 });
--- generate.sh
+++ generate.sh
@@ -5,22 +5,20 @@ openapi-generator-cli generate \
 -g typescript-axios \
 --additional-properties=npmName=vrchatapi \
 --git-user-id=vrchatapi \
---git-repo-id=vrchatapi-node \
+--git-repo-id=vrchatapi-javascript \
 -o . \
 -i ../specification/openapi.yaml \
---http-user-agent="vrchatapi-node"
+--http-user-agent="vrchatapi-javascript"
 
 # Modify package.json
 sed -i 's/Unlicense/MIT/g' ./package.json
 
-# Remove unwanted lines from README
-#sed -i '/Uncomment the following line to set a prefix/d' ./README.md
-#sed -i '/apiKeyPrefix/d' ./README.md
-#sed -i '/Uncomment the following line to set a prefix/d' ./docs/*.md
-#sed -i '/apiKeyPrefix/d' ./docs/*.md
-#sed -i 's/api.deleteUserById/api.getUser/g' ./README.md
+# Enable global cookies
+sed -i '/^import { BASE_PATH/a import axiosCookieJarSupport from "axios-cookiejar-support";axiosCookieJarSupport(globalAxios);import { CookieJar } from "tough-cookie";globalAxios.defaults.jar = new CookieJar();globalAxios.defaults.withCredentials = true;' ./api.ts
 
-#sed -i '/Uncomment the following line to set a prefix/d' ./README.md
+sed -i '/"dependencies"/a     "@types/tough-cookie": "^4.0.1",' ./package.json
+sed -i '/"dependencies"/a     "axios-cookiejar-support": "^1.0.1",' ./package.json
+sed -i '/"dependencies"/a     "tough-cookie": "^4.0.0",' ./package.json
 
 npm install
 npm run build
