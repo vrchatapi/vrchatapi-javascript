15d2c3434569cd8f88e8e6f97ff18707be8b2dd2
Yaakov Schlachter <contact@cgprograms.com>
2025-05-20 15:12:55 -0400

    FIX: cookie examples (#18)
    * Fixed cookie example
    
    * Delete cookies_load.js
    
    * Change `cookies_store.js` to only store
    
    * Create cookies_load.js
    
    * Update cookies_store.js
    
    * Update cookies_load.js
    
    * Fix typo
    
    ---------
    
    Co-authored-by: JelleJurre <76777936+jellejurre@users.noreply.github.com>

--- cookies_load.js
+++ cookies_load.js
@@ -1,20 +1,36 @@
 const vrchat = require("vrchat");
 
+const readline = require("readline")
+
+const tough = require("tough-cookie");
+const fs = require("fs");
+
+const COOKIE_FILE = "cookies.json";
+let cookieJar = new tough.CookieJar();
+
+if (fs.existsSync(COOKIE_FILE)) {
+    const serializedCookies = fs.readFileSync(COOKIE_FILE, "utf-8");
+    cookieJar = tough.CookieJar.deserializeSync(JSON.parse(serializedCookies));
+}
+
 const configuration = new vrchat.Configuration({
     username: "username",
     password: "password",
     baseOptions: {
         headers: {
-            "User-Agent": "ExampleProgram/0.0.1 my@email.com"
-            "Cookie": "auth=[AUTH_COOKIE_HERE]; twoFactorAuth=[TWO_FACTOR_AUTH_COOKIE_HERE]}"
-        }
+            "User-Agent": "ExampleProgram/0.0.1 my@email.com",
+            // Use this instead of jar if you want to hard code cookies
+            // "Cookie": "auth=[AUTH_COOKIE_HERE]; twoFactorAuth=[TWO_FACTOR_AUTH_COOKIE_HERE]"
+        },
+        jar: cookieJar,
     }
 });
 
-const AuthenticationApi = new vrchat.AuthenticationApi(configuration);
+
+const authenticationApi = new AuthenticationApi(configuration);
 
 async function main() {
-    const currentUser = (await AuthenticationApi.getCurrentUser()).data;
+    var currentUser = (await authenticationApi.getCurrentUser()).data
     console.log(`Logged in as: ${currentUser.displayName}`);
 }
 
