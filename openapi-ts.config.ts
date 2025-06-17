3bf04347e2a305a019845c995fdbdaa4f6258e53
Aries <me@ariesclark.com>
2025-06-17 09:17:20 -0600

    fix: error type

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,7 +1,43 @@
+import type { Plugin } from "@hey-api/openapi-ts";
 import { defineConfig } from "@hey-api/openapi-ts";
+import ts from "typescript";
+
+const customPlugin: Plugin.Config<{ name: "vrchat/custom-plugin" }> = {
+	name: "vrchat/custom-plugin",
+	_handler: ({ context }) => {
+		const typesFile = context.file({ id: "types" })!;
+
+		context.subscribe("after", () => {
+			const items = (typesFile as any)._items as Array<ts.Node>;
+
+			// type _Error = { ... }
+			// ->
+			// import { VRChatError } from "../error";
+			// type _Error = VRChatError;
+
+			const errorTypeName = "_Error";
+			const errorTypeIndex = items.findIndex((item: ts.Node) => ts.isTypeAliasDeclaration(item) && item.name.text === errorTypeName);
+			if (errorTypeIndex === -1) throw new Error("Could not find error type definition");
+
+			const { name: errorImport } = typesFile.import({ module: "../error", name: "VRChatError", asType: true });
+			const errorImportIdentifier = ts.factory.createIdentifier(errorImport);
+
+			items[errorTypeIndex] = ts.factory.createTypeAliasDeclaration(
+				undefined,
+				ts.factory.createIdentifier(errorTypeName),
+				undefined,
+				ts.factory.createTypeReferenceNode(
+					errorImportIdentifier,
+					undefined
+				)
+			);
+		});
+	},
+	_handlerLegacy: () => { },
+};
 
 export default defineConfig({
-	input: "https://vrchat.community/openapi.yaml",
+	input: "./openapi.yaml",
 	output: "src/generated",
 	plugins: [
 		{
@@ -25,5 +61,6 @@ export default defineConfig({
 			transformer: true,
 			classNameBuilder: () => "_VRChat",
 		},
+		customPlugin as any
 	],
 });
